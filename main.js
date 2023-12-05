64027ae454d2752eaf52ae51ca87cb7e28fe02fa
Davide Prencipe <davidedev04@gmail.com>
2023-12-05 14:55:00 +0100

    completata esportazione nella pagina

--- main.js
+++ main.js
@@ -75,9 +75,30 @@ posts.forEach(element => {
                 </div>                    
             </div>
         </div>
+    <div class="post__text">
+        ${element.content}
     </div>
-        `
+    <div class="post__image">
+        <img src="${element.media}">
+    </div>
+    <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>
+    </div>
+    `
 
     myCard.innerHTML += createPost;
 
 });
+
+
